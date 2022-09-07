f9221943cef94f51227ced7fd1de6703a21fff62
markkqq <108346728+markkqq@users.noreply.github.com>
2022-09-08 00:00:54 +0700

    Add files via upload

---
+++ lab_1/lab_1/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+namespace lab_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Dictionary<string, double> station = new Dictionary<string, double>();
+
+            station.Add("92", 44);
+            station.Add("95", 46);
+            station.Add("98", 50);
+            station.Add("100", 58);
+            station.Add("дт", 55);
+            bool processing = true;
+
+            ShowInfo();
+            while (processing)
+            {
+                Console.WriteLine("Введите название топлива");
+                string data = Console.ReadLine();
+                data = data.ToLower();
+                if (station.ContainsKey(data))
+                {
+                    Pay(data);
+                }
+                if (!station.ContainsKey(data))
+                {
+                    Console.Clear();
+                    ShowInfo();
+                }
+                if (data == "выход")
+                {
+                    processing = !processing;
+                }
+            }
+            void Pay(string fuel)
+            {
+                int volume;
+                Console.WriteLine("количество");
+                int.TryParse(Console.ReadLine(),out volume);
+                if (volume > 0)
+                {
+                    Console.WriteLine("К оплате "+volume * station[fuel]);
+                }
+                else
+                {
+                    Console.WriteLine("неправильный формат данных");
+                    Console.Clear();
+                    ShowInfo();
+                }
+            }
+            void ShowInfo()
+            {
+                Console.WriteLine("цены на топливо");
+                foreach (KeyValuePair<string, double> fuel in station)
+                {
+                    Console.WriteLine(fuel.Key + " : " + fuel.Value);
+                }
+            }
+        }
+    }
+}
