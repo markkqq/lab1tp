fd944de978e7bb236e9a99eb23977433d0ac0fb4
markkqq <108346728+markkqq@users.noreply.github.com>
2022-09-19 20:46:17 +0700

    Add files via upload
    refactoring

--- lab_1/lab_1/Program.cs
+++ lab_1/lab_1/Program.cs
@@ -1,28 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+
+
 namespace lab_1
 {
     class Program
     {
         static void Main(string[] args)
         {
-
-            List<Fuel> station = new List<Fuel>();
-            Fuel fuel1 = new Fuel("92", 100, 44);
-            Fuel fuel2 = new Fuel("95", 100, 48);
-            Fuel fuel3 = new Fuel("98", 100, 56);
-            Fuel fuel4 = new Fuel("100", 100, 58);
-            Fuel fuel5 = new Fuel("дт", 100, 55);
-            station.Add(fuel1);
-            station.Add(fuel2);
-            station.Add(fuel3);
-            station.Add(fuel4);
-            station.Add(fuel5);
-            bool processing = true;
+            FileToRead fileToRead = new FileToRead(args[0]);
+            Station station = new Station();
+           
+            //bool processing = true;
             
-            ShowInfo();
-            while (processing)
+            station.ShowInfo(); 
+           /* while (processing)
             {
                 Console.WriteLine("Введите название топлива");
                 string data = Console.ReadLine();
@@ -32,7 +25,7 @@ namespace lab_1
                     {
                         if (fuel.ReservedVolume > 0)
                         {
-                            Pay(fuel);
+                            station.Pay(fuel);
                         }
                         else
                         {
@@ -42,71 +35,120 @@ namespace lab_1
                 }
                 
                 
-            }
-            void Pay(Fuel fuel)
-            {
-                int volume;
-                Console.WriteLine("количество");
-                int.TryParse(Console.ReadLine(),out volume);
-                if (volume > 0 & fuel.ReservedVolume-volume>=0)
-                {
-                    Console.WriteLine("К оплате "+volume * fuel.Price);
-                    fuel.ReservedVolume -= volume;
-                    Console.WriteLine("В АЗС осталось"+fuel.ReservedVolume+" литров");
-                    PrintCheck((volume*fuel.Price).ToString());
-                }
-                else
-                {
-                    Console.WriteLine("неправильный формат данных");
-                    Console.Clear();
-                    ShowInfo();
-                }
-            }
-            void ShowInfo()
-            {
-                Console.WriteLine("цены на топливо");
-                foreach (Fuel fuel in station)
-                {
-                    Console.WriteLine(fuel.Name + " : " + fuel.Price);
-                }
-            }
-            void PrintCheck(string s)
-            {
-                StreamWriter sw = new StreamWriter("C:\\Users\\DeminMS\\out.txt");
-                sw.WriteLine(s);
-                sw.Close();
-                
-                
-            }
+            }*/
+            
+           
+            
         }
     }
     class Fuel
     {
         string _name;
         int _reservedvolume;
-        double _price;
         public string Name
         {
-            get { return this._name; }
-            set { this._name = value; }
+            get;
+            set;
 
         }
         public int ReservedVolume
         {
-            get { return this._reservedvolume; }
-            set { this._reservedvolume = value; }
+            get;
+            set;
         }
-        public double Price
-        {
-            get { return this._price; }
-            set { this._price = value; }
-        }
+        public double Price { get; set; }
         public Fuel(string name, int reservedvolume, double price)
         {
             this._name = name;
             this._reservedvolume = reservedvolume;
-            this._price = price;
+            this.Price = price;
         }
     }
-    
+    class Station
+    {
+        private List<Fuel> fuels = new List<Fuel>();
+        public void AddFuel(Fuel fuel)
+        {
+            fuels.Add(fuel);
+        }
+        public void Pay(Fuel fuel)
+        {
+
+            int volume;
+            Console.WriteLine("количество");
+            int.TryParse(Console.ReadLine(), out volume);
+            if (volume > 0 & fuel.ReservedVolume - volume >= 0)
+            {
+                Console.WriteLine("К оплате " + volume * fuel.Price);
+                fuel.ReservedVolume -= volume;
+                Console.WriteLine("В АЗС осталось" + fuel.ReservedVolume + " литров");
+                PrintCheck(
+                    "В " +
+                    DateTime.Now.ToString() + " " +
+                    "Водитель приобрел следующее топливо: " + fuel.Name + "\n" +
+                    "Купил" + volume.ToString() + " литров." + "\n" +
+                    "К оплате " +
+                (volume * fuel.Price).ToString() + "рублей." + "\n"
+                    );
+            }
+            else
+            {
+                Console.WriteLine("неправильный формат данных");
+                Console.Clear();
+                ShowInfo();
+            }
+        }
+        public void PrintCheck(string s)
+        {
+            string path = "C:\\Users\\Андрей\\Desktop\\out.txt";
+            File.AppendAllText(path, s + "\n");
+        }
+        public void ShowInfo()
+        {
+            Console.WriteLine("цены на топливо");
+            foreach (Fuel fuel in fuels)
+            {
+                Console.WriteLine(fuel.Name + " : " + fuel.Price);
+            }
+        }
+    }
+    class FileToRead
+    {
+        private string _path;
+        public FileToRead(string path)
+        {
+            _path = path;
+        }
+        public bool CheckErrors()
+        {
+            bool result = true;
+            StreamReader sr = new StreamReader(_path);
+            string stringtoread = String.Empty;
+            while (stringtoread != null & result)
+            {
+                try
+                {
+                    string s = stringtoread = sr.ReadLine();
+                    int i = int.Parse(sr.ReadLine());
+                    double d = double.Parse(sr.ReadLine());
+                    if (i < 0 || d < 0)
+                    {
+                        result = false;
+                    }
+                }
+                catch
+                {
+                    result = false;
+                }
+            }
+            return result;
+        }
+        public void ReadFile(string _path)
+        {
+            if (CheckErrors())
+            {
+                
+            }
+        }
+    }
 }
