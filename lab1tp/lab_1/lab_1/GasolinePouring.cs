1a0500246288a1bc0a80c0f4c9e901ec1dcc1833
markkqq <108346728+markkqq@users.noreply.github.com>
2022-10-05 10:10:21 +0700

    Add files via upload

---
+++ lab1tp/lab_1/lab_1/GasolinePouring.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_1
+{
+    class GasolinePouring
+    {
+        private int _volume;
+        private Customer _customer;
+        public GasolinePouring(int volume)
+        {
+            this._volume = volume;
+
+        }
+        public Fuel Pour(Fuel fuel)
+        {
+            GasolinePouring pouring = new GasolinePouring(_volume);
+            fuel.ReservedVolume -= _volume;
+            return fuel;
+        }
+
+    }
+}
---
+++ lab1tp/lab_1/lab_1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+
+namespace lab_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Station station = new Station();
+            station.LoadTariffs(args[0]);
+            station.ShowTariffs();
+            station.RunStation();
+        }
+    }
+}
