02f9791578aa268ddb0c1323f7ec9471da26f754
markkqq <108346728+markkqq@users.noreply.github.com>
2022-11-16 12:08:04 +0700

    Add files via upload

---
+++ Repository/FileSaver.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+
+namespace petrolstation
+{
+    public class FileSaver : IFileSaver
+    {
+        private static int fileNumber = 0;
+        public FileSaver()
+        {
+
+        }
+        public void SaveFile(Purchase purchase)
+        {
+            while (File.Exists(string.Format(fileNumber.ToString() + ".txt")))
+            {
+                fileNumber++;
+            }
+
+            StreamWriter streamWriter = new StreamWriter(fileNumber.ToString() + ".txt");
+            streamWriter.WriteLine(@"Клиент приобрел {0} литров {1} топлива. Заплатил {2} рублей. В кошельке осталось {3} рублей. ", purchase.Volume, purchase.Fuel.Name, purchase.Volume * purchase.Fuel.Price, purchase.Client.Wallet.Money);
+            streamWriter.Close();
+        }
+    }
+}
---
+++ petrolstation/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace petrolstation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                UserConsoleInterface userConsoleInterface = new();
+                FileReader fileReader = new FileReader(args);
+                FileSaver fileSaver = new FileSaver();
+                Generator generator = new Generator(fileReader);
+                Station station = new Station(generator, userConsoleInterface, fileSaver);
+                station.Run();
+
+            }
+            else
+            {
+
+            }
+        }
+    }
+    
+}
